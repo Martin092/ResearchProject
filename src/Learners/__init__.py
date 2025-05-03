cdff35cdf502150c70a955c705a6b69335836795
Vivek Kasyap Pasumarthi <V.K.Pasumarthi@student.tudelft.nl>
2025-05-03 16:57:41 +0200

    Update Learners __init__.py file

--- src/Learners/__init__.py
+++ src/Learners/__init__.py
@@ -1,5 +1,6 @@
 from .AbstractLearner import AbstractLearner
 from .ConcreteLearners.ETCLearner import ETCLearner
 from .ConcreteLearners.UCBLearner import UCBLearner
+from .ConcreteLearners.EGreedyLearner import EGreedyLearner
 
-__all__ = ["AbstractLearner", "ETCLearner", "UCBLearner"]
+__all__ = ["AbstractLearner", "ETCLearner", "UCBLearner", "EGreedyLearner"]
