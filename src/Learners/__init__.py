5b07dfef30ae413f5b0fde505ff70461641528f0
Vivek Kasyap Pasumarthi <V.K.Pasumarthi@student.tudelft.nl>
2025-05-04 12:57:35 +0200

    rebase branch to updated main

--- src/Learners/__init__.py
+++ src/Learners/__init__.py
@@ -2,5 +2,6 @@ from .AbstractLearner import AbstractLearner
 from .ConcreteLearners.ETCLearner import ETCLearner
 from .ConcreteLearners.LinUCBLearner import LinUCBLearner
 from .ConcreteLearners.UCBLearner import UCBLearner
+from .ConcreteLearners.EGreedyLearner import EGreedyLearner
 
-__all__ = ["AbstractLearner", "ETCLearner", "LinUCBLearner", "UCBLearner"]
+__all__ = ["AbstractLearner", "ETCLearner", "LinUCBLearner", "UCBLearner, EGreedyLearner"]
